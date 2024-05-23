f2aaa0dc89ce19e5ee0086424c5be75b69ec6a89
Jai <jai@renewablemobile.com.au>
2024-05-23 21:04:06 +1000

    change

--- Ecom/src/components/TheWelcome.vue
+++ Ecom/src/components/TheWelcome.vue
@@ -7,15 +7,15 @@
     </p>
 
     <div class="card">
-        <DataTable :value="customers" paginator :rows="5" :rowsPerPageOptions="[5, 10, 20, 50]" tableStyle="min-width: 50rem"
+        <DataTable v-model:selection="selectedProduct" :value="customers" paginator :rows="5" :rowsPerPageOptions="[5, 10, 20, 50]" tableStyle="min-width: 50rem"
                 paginatorTemplate="RowsPerPageDropdown FirstPageLink PrevPageLink CurrentPageReport NextPageLink LastPageLink"
                 currentPageReportTemplate="{first} to {last} of {totalRecords}">
-            <template #paginatorstart>
-                <Button type="button" icon="pi pi-refresh" text />
+                <template #header>
+                <div style="text-align: left">
+                    <Button icon="pi pi-external-link" label="Export" @click="" />
+                </div>
             </template>
-            <template #paginatorend>
-                <Button type="button" icon="pi pi-download" text />
-            </template>
+            <Column selectionMode="multiple" headerStyle="width: 3rem"></Column>
             <Column field="model" header="Model" sortable style="width: 25%"></Column>
             <Column field="qty" header="QTY" sortable style="width: 25%"></Column>
         </DataTable>
@@ -34,8 +34,10 @@ import ColumnGroup from 'primevue/columngroup';
 import Row from 'primevue/row';
 import { ref, onMounted } from 'vue';
 
-const customers = ref([{"id":1,"model":"iPhone 11","qty":1},{"id":2,"model":"iPhone 12","qty":4}]);
+const customers = ref([{"id":1,"model":"iPhone 11","qty":1},{"id":2,"model":"iPhone 12","qty":4},{"id":3,"model":"iPhone 11","qty":1},{"id":4,"model":"iPhone 12","qty":4},{"id":5,"model":"iPhone 11","qty":1},{"id":6,"model":"iPhone 12","qty":4},{"id":7,"model":"iPhone 11","qty":1},{"id":8,"model":"iPhone 12","qty":4},{"id":9,"model":"iPhone 11","qty":1},{"id":10,"model":"iPhone 12","qty":4}]);
+const selectedProduct = ref();
 const Counta = ref(0);
+
 
 function Test(){
   Counta.value = Counta.value+1
