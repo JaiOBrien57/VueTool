e7011aa324a84c464f918a1f27a047d8c37f9f81
JaiObrien57 <jai@renewablemobile.com.au>
2024-05-24 12:54:09 +1000

    changes

--- Ecom/src/components/TheWelcome.vue
+++ Ecom/src/components/TheWelcome.vue
@@ -1,7 +1,11 @@
 <!-- https://primevue.org/datatable/ -->
 
 <template>
-  <Button label="Submit" @click="Test"/>
+  <div class="card flex justify-content-center">
+        <Toast />
+        <FileUpload mode="basic" name="demo[]" url="/api/upload" accept="csv/*" :maxFileSize="1000000" @upload="onUpload" />
+    </div>
+  <Button label="Submit" @click="test2" />
   <p>
     Count:{{ Counta }}
     </p>
@@ -31,12 +35,19 @@ import DataTable from 'primevue/datatable';
 import Column from 'primevue/column';
 import ColumnGroup from 'primevue/columngroup';  
 import Row from 'primevue/row';
+import FileUpload from 'primevue/fileupload';
+import Toast from 'primevue/toast';
+import { useToast } from "primevue/usetoast";
 import { ref, onMounted } from 'vue';
 
 const customers = ref([{"id":1,"model":"iPhone 11","qty":1},{"id":2,"model":"iPhone 12","qty":4},{"id":3,"model":"iPhone 11","qty":1},{"id":4,"model":"iPhone 12","qty":4},{"id":5,"model":"iPhone 11","qty":1},{"id":6,"model":"iPhone 12","qty":4},{"id":7,"model":"iPhone 11","qty":1},{"id":8,"model":"iPhone 12","qty":4},{"id":9,"model":"iPhone 11","qty":1},{"id":10,"model":"iPhone 12","qty":4}]);
 const selectedProduct = ref();
 const Counta = ref(0);
+const toast = useToast();
 
+const onUpload = () => {
+    toast.add({ severity: 'info', summary: 'Success', detail: 'File Uploaded', life: 3000 });
+};
 
 function Test(){
   Counta.value = Counta.value+1
